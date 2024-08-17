819a8b28af7e7e82133a358ff2798e68f4d5d642
Jonathan Kofi Donkor <jkd6735@gmail.com>
2024-08-17 02:30:54 +0000

    update

--- src/screens/onboard/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Start } from "./Start";
-export { default as Onboard1 } from "./Onboard1";
-export { default as Onboard2 } from "./Onboard2";
-export { default as Onboard3 } from "./Onboard3";
-export { default as Welcome } from "./Welcome";
