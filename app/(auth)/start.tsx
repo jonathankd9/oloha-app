07262109d81c17cf0fb07a7a7f5197ddc77c68b5
Jonathan Kofi Donkor <jkd6735@gmail.com>
2024-08-18 12:29:14 +0000

    commit

---
+++ app/(auth)/start.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const start = () => {
+	return (
+		<View>
+			<Text>start</Text>
+		</View>
+	);
+};
+
+export default start;
+
+const styles = StyleSheet.create({});
--- app/index.tsx
+++ app/index.tsx
@@ -2,15 +2,19 @@ import { Image, View, Text } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Logo from "../assets/images/oloha-logo.png";
 
 const RootPage = () => {
-  return (
-    <SafeAreaView className="flex-1 bg-primary justify-center items-center">
-      <View>
-        <Text className="text-7xl text-main">Hello</Text>
-      </View>
-    </SafeAreaView>
-  );
+	return (
+		<SafeAreaView className="flex-1 bg-primary justify-center items-center">
+			<View>
+				<Image source={Logo} className="mb-5" />
+
+				<Text className="text-4xl text-main">Hello</Text>
+				<Link href={"/"}> Go to </Link>
+			</View>
+		</SafeAreaView>
+	);
 };
 
 export default RootPage;
