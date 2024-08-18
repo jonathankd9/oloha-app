77682ec5fd13d505855c877106a68f8931cfcb99
nii_okantey <nunoogideon19@gmail.com>
2024-08-18 11:49:41 +0000

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./App.{js,jsx,ts,tsx}",
-		"./<custom directory>/**/*.{js,jsx,ts,tsx}",
-		"/app/index.tsx",
-	],
+	content: ["./app/**/*.{js,jsx,ts,tsx}"],
 	theme: {
 		colors: {
 			primary: "#FEBD2F",
